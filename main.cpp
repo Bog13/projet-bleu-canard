23971986d24ef5241891b06dfd24d8b5e9853a41
berenger.ossete@gmail.com <berenger.ossete@gmail.com@98bd73e5-8ada-ae99-cd7c-40d63cfacdd7>
2014-05-26 08:31:56 +0000

    [DEBUT CONCEPTION]

--- main.cpp
+++ main.cpp
@@ -4,9 +4,6 @@ using namespace std;
 
 int main()
 {
-
-    cout << " You bastard, you're not a ducking duk ! " << endl;
     cout << "Hello world!" << endl;
-    cout<<"Test"<<endl;
     return 0;
 }
